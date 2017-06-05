6244582cd67ecb4e4ec002c45def1cc521cdf742
Abdullah Clute <clute.abdullahb@gmail.com>
2017-06-05 04:53:25 -0500

    Added deletion and updating fun

--- section5/lesson1/index.php
+++ section5/lesson1/index.php
@@ -9,18 +9,23 @@
 
   $post = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 
+  if ($_POST['delete']) {
+    $delete_id =  $_POST['delete_id'];
+    $database->query('DELETE FROM posts WHERE id=:id');
+    $database->bind(':id', $delete_id);
+    $database->execute();
+  }
+
   if ($post['submit']) {
+    $id = $post['id'];
     $title = $post['title'];
     $body = $post['body'];
 
-    $database->query('INSERT INTO posts (title, body) VALUES(:title, :body)');
+    $database->query('UPDATE posts SET title=:title, body=:body WHERE id=:id');
+    $database->bind(':id', $id);
     $database->bind(':title', $title);
     $database->bind(':body', $body);
     $database->execute();
-
-    if($database->lastInsertId) {
-      echo '<p>Post Added</p>';
-    }
   }
 
   $database->query('SELECT * FROM posts');
@@ -29,6 +34,9 @@
 
 <h1>Add post</h1>
 <form method="post" action="<?php $_SERVER['PHP_SELF']; ?>">
+  <label>Text ID</label><br />
+  <input type="text" name="id" placeholder="Specify ID"/>
+  <br /><br />
   <label>Text Title</label><br />
   <input type="text" name="title" placeholder="Add a title..."/>
   <br /><br />
@@ -46,5 +54,10 @@
       <h3><?php echo $row['title']; ?></h3>
       <p><?php echo $row['body']; ?></p>
       <h4><?php echo $row['create_date']; ?></h4>
+      <form method='post' action="<?php $_SERVER['PHP_SELF']; ?>">
+        <input type="hidden" name="delete_id" value="<?php echo $row['id']; ?>"/>
+        <input type="submit" name="delete" value='Delete'/>
+      </form>
+    </div>
   <?php endforeach; ?>
 </div>
