1e933500ba7346130f231c05f827c478cc6b73fc
Abdullah Clute <clute.abdullahb@gmail.com>
2017-06-04 19:00:31 -0500

    Created new functions

--- section5/lesson1/index.php
+++ section5/lesson1/index.php
@@ -5,4 +5,17 @@
 
   $database = new Database();
 
+  $database->query('SELECT * FROM posts WHERE id = :id');
+  $database->bind(':id', 2);
+  $rows = $database->resultSet();
+
 ?>
+<h1>Posts</h1>
+<div>
+  <?php foreach($rows as $row) : ?>
+    <div>
+      <h3><?php echo $row['title']; ?></h3>
+      <p><?php echo $row['body']; ?></p>
+      <h4><?php echo $row['create_date']; ?></h4>
+  <?php endforeach; ?>
+</div>
